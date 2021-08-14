69c54db6b9e880a18096da77be6a4f62aedf44dd
SigmaBigLeecher <51486191+SigmaBigLeecher@users.noreply.github.com>
2021-08-14 05:29:24 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,55 +1,52 @@
 -- I hate lua, let's switch to c++ 
+-- I wish i could just edit max player speed/accel var
 
-function init()	
-end
-
-local debugBreak = 0
-
--- Called once every tick
-function tick(dt)
+-- Called once every fixed tick, 60tps
+function update(dt)
     -- don't update in vehicles
     if GetPlayerVehicle() ~= 0 then
-        debugBreak = 1
         do return end
     end
 
     if not InputDown("shift") then
-        debugBreak = 2
         do return end
     end
 
     if not InputDown("up")  then
-        debugBreak = 3
         do return end
     end
 
     velocity = GetPlayerVelocity()
 
-    -- don't apply if our velocity is too low
-    if math.abs(velocity[1]) < 4.8 and math.abs(velocity[3]) < 4.8 then
-        debugBreak = 4
-        do return end
-    end
-
     -- ghetto falling check
     if velocity[2] ~= 0 then
-        debugBreak = 5
         do return end
     end 
 
-    Direction = VecNormalize(velocity)
-    velocity = VecScale(Direction, 10)
+    -- don't apply if our velocity is too low, use case: wall in front of our face
+    if VecLength(velocity) < 6.5 then
+        do return end
+    end
+
+    --
+    if VecLength(velocity) > 8.5 then
+        do return end
+    end
+
+    -- this is ghetto, but I can't just get localplayer eye angles
+    x, y = UiGetMousePos()
+    Direction = UiPixelToWorld(x, y)
+
+    -- default movement velocity is 7, this will increase it to 8.5
+    -- giving us nice 21% speed boost
+    velocity = VecScale(Direction, 9)
 
     velocity[2] = GetPlayerVelocity()[2]
         
     SetPlayerVelocity(velocity)
 end
 
--- Called at a fixed update rate, 60tps
-function update(dt)
-end
-
-
+-- debug, don't upload to workshop this bs
 function draw()
     UiPush()
 	UiAlign("center middle")
@@ -57,7 +54,18 @@ function draw()
     UiFont("bold.ttf", 24)
     velocity = GetPlayerVelocity()
     UiText(tostring(velocity[1]), true)
+    UiText(tostring(velocity[2]), true)
+    velocity[2] = 0
     UiText(tostring(velocity[3]), true)
-    UiText(tostring(debugBreak), true)
+    UiText(tostring(VecLength(velocity)), true)
+
+    x, y = UiGetMousePos()
+    dir = UiPixelToWorld(x, y)
+    veldir = VecNormalize(velocity)
+
+    UiText(tostring(x), true)
+    UiText(tostring(y), true)
+    UiText(tostring(dir[1]), true)
+    UiText(tostring(veldir[1]), true)
     UiPop()
 end
